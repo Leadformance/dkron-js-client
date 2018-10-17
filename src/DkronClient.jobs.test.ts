f8be1baa1abcff42ba90e380405be4efb75d72f9
Julien Giovaresco <dev@giovaresco.fr>
2018-10-17 11:05:32 +0200

    refactor(jobs): prevent duplicated code in tests

--- src/DkronClient.jobs.test.ts
+++ src/DkronClient.jobs.test.ts
@@ -2,6 +2,27 @@ import { createDkronClient, CreateJobRequest, IDkronClient } from 'src';
 import { newConfig } from 'test/fixtures';
 
 describe('DkronClient - Jobs API', () => {
+  const BASE_EXPECTED_JOB = {
+    command: '',
+    concurrency: 'allow',
+    dependent_jobs: null,
+    disabled: false,
+    environment_variables: null,
+    error_count: 0,
+    executor: 'shell',
+    owner: '',
+    owner_email: '',
+    parent_job: '',
+    processors: null,
+    retries: 0,
+    schedule: '0 0 1 * *',
+    shell: false,
+    status: 'success',
+    success_count: 0,
+    tags: null,
+    timezone: '',
+  };
+
   let dkronClient: IDkronClient;
 
   beforeEach(() => {
@@ -21,28 +42,13 @@ describe('DkronClient - Jobs API', () => {
 
       const result = await dkronClient.createJob(body);
       expect(result).toMatchObject({
-        command: '',
-        concurrency: 'allow',
-        dependent_jobs: null,
-        disabled: false,
-        environment_variables: null,
-        error_count: 0,
-        executor: 'shell',
+        ...BASE_EXPECTED_JOB,
         executor_config: {
           command: 'echo "Hello from parent"',
         },
         name: 'my-job',
-        owner: '',
-        owner_email: '',
-        parent_job: '',
-        processors: null,
-        retries: 0,
         schedule: '@every 1m',
-        shell: false,
         status: '',
-        success_count: 0,
-        tags: null,
-        timezone: '',
       });
     });
   });
@@ -51,67 +57,28 @@ describe('DkronClient - Jobs API', () => {
     it('return the job', async () => {
       const result = await dkronClient.getJob('job_1');
       expect(result).toMatchObject({
-        command: '',
-        concurrency: 'allow',
-        dependent_jobs: null,
-        disabled: false,
-        environment_variables: null,
-        error_count: 0,
-        executor: 'shell',
+        ...BASE_EXPECTED_JOB,
         executor_config: {
           command: 'echo "Hello from job_1"',
         },
         name: 'job_1',
-        owner: '',
-        owner_email: '',
-        parent_job: '',
-        processors: null,
-        retries: 0,
         schedule: '0 0 1 * *',
-        shell: false,
-        status: 'success',
-        success_count: 0,
-        tags: null,
-        timezone: '',
       });
     });
   });
 
   describe('getJobs', () => {
     it('return all jobs', async () => {
-      const expectedJob = {
-        command: '',
-        concurrency: 'allow',
-        dependent_jobs: null,
-        disabled: false,
-        environment_variables: null,
-        error_count: 0,
-        executor: 'shell',
-        executor_config: {},
-        name: 'job_1',
-        owner: '',
-        owner_email: '',
-        parent_job: '',
-        processors: null,
-        retries: 0,
-        schedule: '0 0 1 * *',
-        shell: false,
-        status: 'success',
-        success_count: 0,
-        tags: null,
-        timezone: '',
-      };
-
       const results = await dkronClient.getJobs();
       expect(results).toEqual(
         expect.arrayContaining([
           expect.objectContaining({
-            ...expectedJob,
+            ...BASE_EXPECTED_JOB,
             name: 'job_1',
             schedule: '0 0 1 * *',
           }),
           expect.objectContaining({
-            ...expectedJob,
+            ...BASE_EXPECTED_JOB,
             name: 'job_2',
             schedule: '0 0 2 * *',
           }),
